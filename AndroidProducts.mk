cee6624814d1248ef9eee77a0f6d49ef0169a3a5
Pomelohan <2652609017@qq.com>
2021-11-19 17:44:52 +0800

    OP4EA7: Initial Reno 5 Pro+ Tree Bringup
    Signed-off-by: Pomelohan <2652609017@qq.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_OP4EA7.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_OP4EA7-user \
+    omni_OP4EA7-userdebug \
+    omni_OP4EA7-eng
---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_OP4EA7-user
+add_lunch_combo omni_OP4EA7-userdebug
+add_lunch_combo omni_OP4EA7-eng
