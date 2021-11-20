107d07ae93247273b6d341d6f275ccd18d396024
GarfieldHan <2652609017@qq.com>
2021-11-21 00:25:28 +0800

    OP4EA7: Enable Fastbootd Feature
    Signed-off-by: GarfieldHan <2652609017@qq.com>

--- device.mk
+++ device.mk
@@ -9,3 +9,11 @@ LOCAL_PATH := device/oppo/OP4EA7
 PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-impl-wrapper.recovery \
